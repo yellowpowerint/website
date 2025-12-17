f420eaf5bc67830bbf23dfc885c163176f10427a
webblabsorg <webblabsorg@gmail.com>
2025-12-17 02:22:33 +0000

    Redirect /favicon.ico to custom favicon

--- dev/middleware.ts
+++ dev/middleware.ts
@@ -7,6 +7,12 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === '/favicon.ico') {
+    const url = request.nextUrl.clone();
+    url.pathname = '/images/favicon.jpg';
+    return NextResponse.redirect(url, 308);
+  }
+
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set('x-pathname', request.nextUrl.pathname);
 
@@ -18,5 +24,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: '/admin/:path*',
+  matcher: ['/admin/:path*', '/favicon.ico'],
 };
